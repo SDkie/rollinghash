6b8f22c6bdca053122e47794874b24ae5acb94d1
Kumar Sukhani <kumarsukhani@gmail.com>
2023-02-08 16:40:23 +0530

    Added check for input file size in signature

--- pkg/signature/signature.go
+++ pkg/signature/signature.go
@@ -2,6 +2,7 @@ package signature
 
 import (
 	"encoding/binary"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -14,28 +15,34 @@ import (
 func GenerateSignature(inputFileName, outputFileName string) error {
 	infile, err := os.Open(inputFileName)
 	if err != nil {
-		log.Printf("Error opening InputFile: %s", err)
+		log.Printf("error opening input file: %s", err)
 		return err
 	}
 	defer infile.Close()
 
 	outfile, err := os.Create(outputFileName)
 	if err != nil {
-		log.Printf("Error creating SignatureFile: %s", err)
+		log.Printf("error creating signature file: %s", err)
 		return err
 	}
 	defer outfile.Close()
 
 	stats, err := infile.Stat()
 	if err != nil {
-		log.Printf("Error getting file stats: %s", err)
+		log.Printf("error getting file stats: %s", err)
 		return err
 	}
 
 	fileSize := stats.Size()
 	chunkSize := getOptimalChunkSize(fileSize)
 
-	util.WriteUint32InHex(outfile, uint32(chunkSize))
+	if fileSize == 0 {
+		err := fmt.Errorf("input filesize is 0")
+		log.Println(err)
+		return err
+	}
+
+	err = util.WriteUint32InHex(outfile, uint32(chunkSize))
 	if err != nil {
 		return err
 	}
@@ -51,7 +58,7 @@ func GenerateSignature(inputFileName, outputFileName string) error {
 			if err == io.EOF {
 				break
 			}
-			log.Printf("Error reading file: %s", err)
+			log.Printf("error reading file: %s", err)
 			return err
 		}
 
@@ -77,7 +84,7 @@ type Signature struct {
 func ReadSignature(sigFileName string) (*Signature, error) {
 	infile, err := os.Open(sigFileName)
 	if err != nil {
-		log.Printf("Error opening SignatureFile: %s", err)
+		log.Printf("error opening signature File: %s", err)
 		return nil, err
 	}
 	defer infile.Close()
@@ -87,7 +94,7 @@ func ReadSignature(sigFileName string) (*Signature, error) {
 
 	_, err = infile.Read(data)
 	if err != nil {
-		log.Printf("Error reading file: %s", err)
+		log.Printf("error reading signature file: %s", err)
 		return nil, err
 	}
 	signature.ChunkLen = binary.BigEndian.Uint32(data)
