878ac4e152718efe23f6dfcb37d1b393e61c556e
Kumar Sukhani <kumarsukhani@gmail.com>
2023-02-11 21:13:01 +0530

    moved testdata incide delta and signature package

--- pkg/delta/delta_test.go
+++ pkg/delta/delta_test.go
@@ -6,17 +6,9 @@ import (
 	"testing"
 
 	"github.com/SDkie/rollinghash/pkg/delta"
-	"github.com/SDkie/rollinghash/pkg/signature"
 	"github.com/SDkie/rollinghash/pkg/util"
 	"github.com/google/uuid"
 )
-
-type Case struct {
-	InputFileName   string
-	SigFileName     string
-	UpdatedFileName string
-	DeltaFileName   string
-}
 
 // TestFiles format
 // TestX.org    : Original file
@@ -24,105 +16,69 @@ type Case struct {
 // TestX.update : Updated file
 // TestX.delta  : Delta file
 
-// Infomations about test cases
-// Test1 : One Chunk file with no changes
-// Test2 : One Chunk file with literals at start
-// Test3 : One Chunk file with literals at end
+func TestGenerateDelta(t *testing.T) {
+	cases := []struct {
+		name   string
+		testNo int
+		err    error
+	}{
+		// Happy Paths
+		{name: "One Chunk file with no changes", testNo: 1, err: nil},
+		{name: "One Chunk file with literals at start", testNo: 2, err: nil},
+		{name: "One Chunk file with literals at end", testNo: 3, err: nil},
 
-// Test4 : Two Chunk file with no changes
-// Test5 : Two Chunk file with literals at start
-// Test6 : Two Chunk file with literals at middle
-// Test7 : Two Chunk file with literals at end
-// Test8 : Two Chunk file with literals at start, middle and end
-// Test9 : Two Chunk file with trimmed first chunk
-// Test10 : Two Chunk file with some chars replaced in first chunk
-// Test11 : Two Chunk file with chunk swapped
-// Test12 : Two Chunk file with duplicate chunks in updated file
-// Test13 : Two Chunk file with missing first chunk in updated file
-// Test14 : Two Chunk file with missing second chunk in updated file
-// Test15 : Two Chunk file with updated file having no common data
+		{name: "Two Chunk file with no changes", testNo: 4, err: nil},
+		{name: "Two Chunk file with literals at start", testNo: 5, err: nil},
+		{name: "Two Chunk file with literals at middle", testNo: 6, err: nil},
+		{name: "Two Chunk file with literals at end", testNo: 7, err: nil},
+		{name: "Two Chunk file with literals at start, middle and end", testNo: 8, err: nil},
+		{name: "Two Chunk file with trimmed first chunk", testNo: 9, err: nil},
+		{name: "Two Chunk file with some chars replaced in first chunk", testNo: 10, err: nil},
+		{name: "Two Chunk file with chunk swapped", testNo: 11, err: nil},
+		{name: "Two Chunk file with duplicate chunks in updated file", testNo: 12, err: nil},
+		{name: "Two Chunk file with missing first chunk in updated file", testNo: 13, err: nil},
+		{name: "Two Chunk file with missing second chunk in updated file", testNo: 14, err: nil},
+		{name: "Two Chunk file with updated file having no common data", testNo: 15, err: nil},
 
-// Test16 : Small Chunk with some literals at the start
-// Test17 : Small Chunk with some literals at the end
+		{name: "Small Chunk with some literals at the start", testNo: 16, err: nil},
+		{name: "Small Chunk with some literals at the end", testNo: 17, err: nil},
 
-// Test18 : Large Chunk with some literals at the start
-// Test19 : Large Chunk with some literals at the end
-// Test20 : Large Chunk with some literals missing in the middle
+		{name: "Large Chunk with some literals at the start", testNo: 18, err: nil},
+		{name: "Large Chunk with some literals at the end", testNo: 19, err: nil},
+		{name: "Large Chunk with some literals missing in the middle", testNo: 20},
 
-// Error Cases:
-// Test100 : Empty input file for signature
-// Test101 : Empty original file for delta
-// Test102 : Invalid signature file for delta
-// Test103 : Invalid chunk size
-// Test104 : Empty updated file
+		// Unhappy Paths
+		{name: "Empty Original file", testNo: 101, err: delta.ErrEmptyOriginalFile},
+		{name: "Empty Updated file", testNo: 102, err: delta.ErrEmptyUpdatedFile},
+	}
 
-func TestDelta(t *testing.T) {
-	var cases []Case
-	for i := 1; i <= 20; i++ {
-		c := Case{
-			InputFileName:   fmt.Sprintf("../testdata/test%d.org", i),
-			SigFileName:     fmt.Sprintf("../testdata/test%d.sig", i),
-			UpdatedFileName: fmt.Sprintf("../testdata/test%d.update", i),
-			DeltaFileName:   fmt.Sprintf("../testdata/test%d.delta", i),
+	for _, c := range cases {
+		tf := func(t *testing.T) {
+			inputfile := fmt.Sprintf("testdata/test%d.org", c.testNo)
+			sigfile := fmt.Sprintf("testdata/test%d.sig", c.testNo)
+			updatedfile := fmt.Sprintf("testdata/test%d.update", c.testNo)
+			expectedDeltafile := fmt.Sprintf("testdata/test%d.delta", c.testNo)
+
+			deltafile := fmt.Sprintf("testdata/%s.delta", uuid.New().String())
+			defer os.Remove(deltafile)
+
+			err := delta.GenerateDelta(inputfile, sigfile, updatedfile, deltafile)
+			if err != c.err {
+				t.Fatalf("'%s' Failed : expected error:%v, got:%v", t.Name(), c.err, err)
+			}
+			if err != nil {
+				return
+			}
+
+			match, err := util.CompareFileContents(deltafile, expectedDeltafile)
+			if err != nil {
+				t.Fatalf("'%s' Failed with error: %v", t.Name(), err)
+			}
+			if !match {
+				t.Fatalf("'%s' Failed : delta file contents do not match", t.Name())
+			}
 		}
-		cases = append(cases, c)
-	}
 
-	for i, c := range cases {
-		outfile := fmt.Sprintf("%s.sig", uuid.New().String())
-		err := delta.GenerateDelta(c.InputFileName, c.SigFileName, c.UpdatedFileName, outfile)
-		if err != nil {
-			t.Errorf("Test%d failed: error generating delta: %s", i+1, err)
-		}
-
-		match, err := util.CompareFileContents(outfile, c.DeltaFileName)
-		if err != nil {
-			t.Errorf("Test%d failed: error comparing files: %s", i+1, err)
-		}
-		if !match {
-			t.Errorf("Test%d failed: delta mismatch for testfile:%s", i+1, c.InputFileName)
-		}
-
-		os.Remove(outfile)
-	}
-}
-
-func TestEmptyOriginalFile(t *testing.T) {
-	outfile := fmt.Sprintf("%s.sig", uuid.New().String())
-	defer os.Remove(outfile)
-
-	err := delta.GenerateDelta("../testdata/Test101.org", "../testdata/Test101.sig", "../testdata/Test101.update", outfile)
-	if err == nil || err != delta.ErrEmptyOriginalFile {
-		t.Fatalf("Test should fail with error:%s", delta.ErrEmptyOriginalFile)
-	}
-}
-
-func TestInvalidSignatureFile(t *testing.T) {
-	outfile := fmt.Sprintf("%s.sig", uuid.New().String())
-	defer os.Remove(outfile)
-
-	err := delta.GenerateDelta("../testdata/Test102.org", "../testdata/Test102.sig", "../testdata/Test102.update", outfile)
-	if err == nil || err != signature.ErrInvalidSignatureFile {
-		t.Fatalf("Test should fail with error:%s", signature.ErrInvalidSignatureFile)
-	}
-}
-
-func TestInvalidChunkSize(t *testing.T) {
-	outfile := fmt.Sprintf("%s.sig", uuid.New().String())
-	defer os.Remove(outfile)
-
-	err := delta.GenerateDelta("../testdata/Test103.org", "../testdata/Test103.sig", "../testdata/Test103.update", outfile)
-	if err == nil || err != signature.ErrInvalidChunkSize {
-		t.Fatalf("Test should fail with error:%s", signature.ErrInvalidChunkSize)
-	}
-}
-
-func TestEmptyUpdatedFile(t *testing.T) {
-	outfile := fmt.Sprintf("%s.sig", uuid.New().String())
-	defer os.Remove(outfile)
-
-	err := delta.GenerateDelta("../testdata/Test104.org", "../testdata/Test104.sig", "../testdata/Test104.update", outfile)
-	if err == nil || err != delta.ErrEmptyUpdatedFile {
-		t.Fatalf("Test should fail with error:%s", delta.ErrEmptyUpdatedFile)
+		t.Run(c.name, tf)
 	}
 }
--- pkg/signature/signature_test.go
+++ pkg/signature/signature_test.go
@@ -3,6 +3,7 @@ package signature_test
 import (
 	"fmt"
 	"os"
+	"reflect"
 	"testing"
 
 	"github.com/SDkie/rollinghash/pkg/signature"
@@ -10,47 +11,90 @@ import (
 	"github.com/google/uuid"
 )
 
-type Case struct {
-	InputFileName       string
-	ExpectedSigFileName string
-}
+// TestFiles format
+// TestX.org : Input file
+// TestX.sig : Signature file
 
-func TestSignature(t *testing.T) {
-	// NOTE: check delta_test.go for test case details
-	var cases []Case
-	for i := 1; i <= 20; i++ {
-		c := Case{
-			InputFileName:       fmt.Sprintf("../testdata/test%d.org", i),
-			ExpectedSigFileName: fmt.Sprintf("../testdata/test%d.sig", i),
-		}
-		cases = append(cases, c)
+func TestGenerateSignature(t *testing.T) {
+	cases := []struct {
+		name   string
+		testNo int
+		err    error
+	}{
+		// Happy Paths
+		{name: "One Chunk file", testNo: 1, err: nil},
+		{name: "Two Chunk file", testNo: 2, err: nil},
+		{name: "Three Chunk file", testNo: 3, err: nil},
+		{name: "Small Chunk file", testNo: 4, err: nil},
+		{name: "Big Chunk file", testNo: 5, err: nil},
+
+		// Unhappy Paths
+		{name: "Empty Input file", testNo: 101, err: signature.ErrEmptyInputFile},
 	}
 
-	for i, c := range cases {
-		outfile := fmt.Sprintf("%s.sig", uuid.New().String())
-		_, err := signature.GenerateSignature(c.InputFileName, outfile)
-		if err != nil {
-			t.Errorf("Test%d failed: error generating signature: %s", i+1, err)
+	for _, c := range cases {
+		tf := func(t *testing.T) {
+			inputfile := fmt.Sprintf("testdata/test%d.org", c.testNo)
+			expectedSigfile := fmt.Sprintf("testdata/test%d.sig", c.testNo)
+
+			sigfile := fmt.Sprintf("testdata/%s.delta", uuid.New().String())
+			defer os.Remove(sigfile)
+
+			_, err := signature.GenerateSignature(inputfile, sigfile)
+			if err != c.err {
+				t.Fatalf("'%s' Failed : expected error:%v, got:%v", t.Name(), c.err, err)
+			}
+			if err != nil {
+				return
+			}
+
+			match, err := util.CompareFileContents(sigfile, expectedSigfile)
+			if err != nil {
+				t.Fatalf("'%s' Failed with error : %v", t.Name(), err)
+			}
+			if !match {
+				t.Fatalf("'%s' Failed : signature file contents do not match", t.Name())
+			}
 		}
 
-		match, err := util.CompareFileContents(outfile, c.ExpectedSigFileName)
-		if err != nil {
-			t.Errorf("Test%d failed: error comparing files: %s", i+1, err)
-		}
-		if !match {
-			t.Errorf("Test%d failed: signature mismatch for testfile:%s", i+1, c.InputFileName)
-		}
-
-		os.Remove(outfile)
+		t.Run(c.name, tf)
 	}
 }
 
-func TestEmptyInputFile(t *testing.T) {
-	outfile := fmt.Sprintf("%s.sig", uuid.New().String())
-	defer os.Remove(outfile)
+func TestReadSignature(t *testing.T) {
+	cases := []struct {
+		name      string
+		testNo    int
+		signature signature.Signature
+		err       error
+	}{
+		// Happy Paths
+		{name: "One Chunk file", testNo: 1, signature: signature.Signature{ChunkLen: 256, TotalChunks: 1, Hashes: []uint32{3963550426}}, err: nil},
+		{name: "Two Chunk file", testNo: 2, signature: signature.Signature{ChunkLen: 256, TotalChunks: 2, Hashes: []uint32{3963550426, 1999309273}}, err: nil},
+		{name: "Three Chunk file", testNo: 3, signature: signature.Signature{ChunkLen: 256, TotalChunks: 3, Hashes: []uint32{3963550426, 1999309273, 35068120}}, err: nil},
+		{name: "Small Chunk file", testNo: 4, signature: signature.Signature{ChunkLen: 256, TotalChunks: 1, Hashes: []uint32{4150264061}}, err: nil},
 
-	_, err := signature.GenerateSignature("../testdata/Test100.org", outfile)
-	if err == nil || err != signature.ErrEmptyInputFile {
-		t.Fatalf("Test should fail with error:%s", signature.ErrEmptyInputFile)
+		// Unhappy Paths
+		{name: "Invalid signature file", testNo: 102, err: signature.ErrInvalidSignatureFile},
+		{name: "Invalid chunk size", testNo: 103, err: signature.ErrInvalidChunkSize},
+	}
+
+	for _, c := range cases {
+		tf := func(t *testing.T) {
+			sigfile := fmt.Sprintf("testdata/test%d.sig", c.testNo)
+			signature, err := signature.ReadSignature(sigfile)
+			if err != c.err {
+				t.Fatalf("'%s' Failed : expected error:%v, got:%v", t.Name(), c.err, err)
+			}
+			if err != nil {
+				return
+			}
+
+			if !reflect.DeepEqual(*signature, c.signature) {
+				t.Fatalf("'%s' Failed : signature does not match", t.Name())
+			}
+		}
+
+		t.Run(c.name, tf)
 	}
 }
