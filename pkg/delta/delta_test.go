c589da923ee4a06888a7369b11147fcd6c3e3548
Kumar Sukhani <kumarsukhani@gmail.com>
2023-02-12 12:50:50 +0530

    changing err to expError in test cases

--- pkg/delta/delta_test.go
+++ pkg/delta/delta_test.go
@@ -18,38 +18,38 @@ import (
 
 func TestGenerateDelta(t *testing.T) {
 	cases := []struct {
-		name   string
-		testNo int
-		err    error
+		name     string
+		testNo   int
+		expError error
 	}{
 		// Happy Paths
-		{name: "One Chunk file with no changes", testNo: 1, err: nil},
-		{name: "One Chunk file with literals at start", testNo: 2, err: nil},
-		{name: "One Chunk file with literals at end", testNo: 3, err: nil},
+		{name: "One Chunk file with no changes", testNo: 1, expError: nil},
+		{name: "One Chunk file with literals at start", testNo: 2, expError: nil},
+		{name: "One Chunk file with literals at end", testNo: 3, expError: nil},
 
-		{name: "Two Chunk file with no changes", testNo: 4, err: nil},
-		{name: "Two Chunk file with literals at start", testNo: 5, err: nil},
-		{name: "Two Chunk file with literals at middle", testNo: 6, err: nil},
-		{name: "Two Chunk file with literals at end", testNo: 7, err: nil},
-		{name: "Two Chunk file with literals at start, middle and end", testNo: 8, err: nil},
-		{name: "Two Chunk file with trimmed first chunk", testNo: 9, err: nil},
-		{name: "Two Chunk file with some chars replaced in first chunk", testNo: 10, err: nil},
-		{name: "Two Chunk file with chunk swapped", testNo: 11, err: nil},
-		{name: "Two Chunk file with duplicate chunks in updated file", testNo: 12, err: nil},
-		{name: "Two Chunk file with missing first chunk in updated file", testNo: 13, err: nil},
-		{name: "Two Chunk file with missing second chunk in updated file", testNo: 14, err: nil},
-		{name: "Two Chunk file with updated file having no common data", testNo: 15, err: nil},
+		{name: "Two Chunk file with no changes", testNo: 4, expError: nil},
+		{name: "Two Chunk file with literals at start", testNo: 5, expError: nil},
+		{name: "Two Chunk file with literals at middle", testNo: 6, expError: nil},
+		{name: "Two Chunk file with literals at end", testNo: 7, expError: nil},
+		{name: "Two Chunk file with literals at start, middle and end", testNo: 8, expError: nil},
+		{name: "Two Chunk file with trimmed first chunk", testNo: 9, expError: nil},
+		{name: "Two Chunk file with some chars replaced in first chunk", testNo: 10, expError: nil},
+		{name: "Two Chunk file with chunk swapped", testNo: 11, expError: nil},
+		{name: "Two Chunk file with duplicate chunks in updated file", testNo: 12, expError: nil},
+		{name: "Two Chunk file with missing first chunk in updated file", testNo: 13, expError: nil},
+		{name: "Two Chunk file with missing second chunk in updated file", testNo: 14, expError: nil},
+		{name: "Two Chunk file with updated file having no common data", testNo: 15, expError: nil},
 
-		{name: "Small Chunk with some literals at the start", testNo: 16, err: nil},
-		{name: "Small Chunk with some literals at the end", testNo: 17, err: nil},
+		{name: "Small Chunk with some literals at the start", testNo: 16, expError: nil},
+		{name: "Small Chunk with some literals at the end", testNo: 17, expError: nil},
 
-		{name: "Large Chunk with some literals at the start", testNo: 18, err: nil},
-		{name: "Large Chunk with some literals at the end", testNo: 19, err: nil},
-		{name: "Large Chunk with some literals missing in the middle", testNo: 20},
+		{name: "Large Chunk with some literals at the start", testNo: 18, expError: nil},
+		{name: "Large Chunk with some literals at the end", testNo: 19, expError: nil},
+		{name: "Large Chunk with some literals missing in the middle", testNo: 20, expError: nil},
 
 		// Unhappy Paths
-		{name: "Empty Original file", testNo: 101, err: delta.ErrEmptyOriginalFile},
-		{name: "Empty Updated file", testNo: 102, err: delta.ErrEmptyUpdatedFile},
+		{name: "Empty Original file", testNo: 101, expError: delta.ErrEmptyOriginalFile},
+		{name: "Empty Updated file", testNo: 102, expError: delta.ErrEmptyUpdatedFile},
 	}
 
 	for _, c := range cases {
@@ -63,8 +63,8 @@ func TestGenerateDelta(t *testing.T) {
 			defer os.Remove(deltafile)
 
 			err := delta.GenerateDelta(inputfile, sigfile, updatedfile, deltafile)
-			if err != c.err {
-				t.Fatalf("'%s' Failed : expected error:%v, got:%v", t.Name(), c.err, err)
+			if err != c.expError {
+				t.Fatalf("'%s' Failed : expected error:%v, got:%v", t.Name(), c.expError, err)
 			}
 			if err != nil {
 				return
--- pkg/signature/signature_test.go
+++ pkg/signature/signature_test.go
@@ -17,19 +17,19 @@ import (
 
 func TestGenerateSignature(t *testing.T) {
 	cases := []struct {
-		name   string
-		testNo int
-		err    error
+		name     string
+		testNo   int
+		expError error
 	}{
 		// Happy Paths
-		{name: "One Chunk file", testNo: 1, err: nil},
-		{name: "Two Chunk file", testNo: 2, err: nil},
-		{name: "Three Chunk file", testNo: 3, err: nil},
-		{name: "Small Chunk file", testNo: 4, err: nil},
-		{name: "Big Chunk file", testNo: 5, err: nil},
+		{name: "One Chunk file", testNo: 1, expError: nil},
+		{name: "Two Chunk file", testNo: 2, expError: nil},
+		{name: "Three Chunk file", testNo: 3, expError: nil},
+		{name: "Small Chunk file", testNo: 4, expError: nil},
+		{name: "Big Chunk file", testNo: 5, expError: nil},
 
 		// Unhappy Paths
-		{name: "Empty Input file", testNo: 101, err: signature.ErrEmptyInputFile},
+		{name: "Empty Input file", testNo: 101, expError: signature.ErrEmptyInputFile},
 	}
 
 	for _, c := range cases {
@@ -41,8 +41,8 @@ func TestGenerateSignature(t *testing.T) {
 			defer os.Remove(sigfile)
 
 			_, err := signature.GenerateSignature(inputfile, sigfile)
-			if err != c.err {
-				t.Fatalf("'%s' Failed : expected error:%v, got:%v", t.Name(), c.err, err)
+			if err != c.expError {
+				t.Fatalf("'%s' Failed : expected error:%v, got:%v", t.Name(), c.expError, err)
 			}
 			if err != nil {
 				return
@@ -63,34 +63,34 @@ func TestGenerateSignature(t *testing.T) {
 
 func TestReadSignature(t *testing.T) {
 	cases := []struct {
-		name      string
-		testNo    int
-		signature signature.Signature
-		err       error
+		name         string
+		testNo       int
+		expSignature signature.Signature
+		expError     error
 	}{
 		// Happy Paths
-		{name: "One Chunk file", testNo: 1, signature: signature.Signature{ChunkLen: 256, TotalChunks: 1, Hashes: []uint32{3963550426}}, err: nil},
-		{name: "Two Chunk file", testNo: 2, signature: signature.Signature{ChunkLen: 256, TotalChunks: 2, Hashes: []uint32{3963550426, 1999309273}}, err: nil},
-		{name: "Three Chunk file", testNo: 3, signature: signature.Signature{ChunkLen: 256, TotalChunks: 3, Hashes: []uint32{3963550426, 1999309273, 35068120}}, err: nil},
-		{name: "Small Chunk file", testNo: 4, signature: signature.Signature{ChunkLen: 256, TotalChunks: 1, Hashes: []uint32{4150264061}}, err: nil},
+		{name: "One Chunk file", testNo: 1, expSignature: signature.Signature{ChunkLen: 256, TotalChunks: 1, Hashes: []uint32{3963550426}}, expError: nil},
+		{name: "Two Chunk file", testNo: 2, expSignature: signature.Signature{ChunkLen: 256, TotalChunks: 2, Hashes: []uint32{3963550426, 1999309273}}, expError: nil},
+		{name: "Three Chunk file", testNo: 3, expSignature: signature.Signature{ChunkLen: 256, TotalChunks: 3, Hashes: []uint32{3963550426, 1999309273, 35068120}}, expError: nil},
+		{name: "Small Chunk file", testNo: 4, expSignature: signature.Signature{ChunkLen: 256, TotalChunks: 1, Hashes: []uint32{4150264061}}, expError: nil},
 
 		// Unhappy Paths
-		{name: "Invalid signature file", testNo: 102, err: signature.ErrInvalidSignatureFile},
-		{name: "Invalid chunk size", testNo: 103, err: signature.ErrInvalidChunkSize},
+		{name: "Invalid signature file", testNo: 102, expError: signature.ErrInvalidSignatureFile},
+		{name: "Invalid chunk size", testNo: 103, expError: signature.ErrInvalidChunkSize},
 	}
 
 	for _, c := range cases {
 		tf := func(t *testing.T) {
 			sigfile := fmt.Sprintf("testdata/test%d.sig", c.testNo)
 			signature, err := signature.ReadSignature(sigfile)
-			if err != c.err {
-				t.Fatalf("'%s' Failed : expected error:%v, got:%v", t.Name(), c.err, err)
+			if err != c.expError {
+				t.Fatalf("'%s' Failed : expected error:%v, got:%v", t.Name(), c.expError, err)
 			}
 			if err != nil {
 				return
 			}
 
-			if !reflect.DeepEqual(*signature, c.signature) {
+			if !reflect.DeepEqual(*signature, c.expSignature) {
 				t.Fatalf("'%s' Failed : signature does not match", t.Name())
 			}
 		}
